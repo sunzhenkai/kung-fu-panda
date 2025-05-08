5df2b54e8916ac9335e28913d33554090e5d9d23
zhenkai.sun <zhenkai.sun@qq.com>
2025-05-09 01:05:02 +0800

    update

--- cmd/main.cc
+++ cmd/main.cc
@@ -4,6 +4,7 @@
 #include "common/flags.h"
 #include "cppcommon/utils/error.h"
 #include "cppcommon/utils/to_str.h"
+#include "service/impl/http_impl.h"
 #include "service/impl/impl.h"
 #include "utils/log.h"
 
@@ -21,6 +22,9 @@ int main(int argc, char** argv) {
   brpc::ServerOptions options;
   kfpanda::KfPandaServiceImpl service;
   cppcommon::Assert(!server.AddService(&service, brpc::SERVER_DOESNT_OWN_SERVICE), "[main] add service failed.");
+  kfpanda::HttpKfPandaServiceImpl http_service;
+  auto mp = "/api/echo => Echo,/api/replay => Replay";
+  cppcommon::Assert(!server.AddService(&service, brpc::SERVER_DOESNT_OWN_SERVICE, mp), "[main] add service failed.");
   cppcommon::Assert(!server.Start(kfpanda::FLAGS_port, &options), "[main] start server failed.");
 
   // 3. waiting
---
+++ src/service/impl/http_impl.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "brpc/closure_guard.h"
+#include "handler/record_handler.h"
+#include "handler/replay_handler.h"
+#include "protos/service/kfpanda/kfpanda.pb.h"
+
+namespace kfpanda {
+class HttpKfPandaServiceImpl : public kfpanda::HttpKfPandaService {
+  void Echo(::google::protobuf::RpcController* controller, const ::kfpanda::HttpRequest* request,
+            ::kfpanda::HttpResponse* response, ::google::protobuf::Closure* done) override;
+  void Replay(::google::protobuf::RpcController* controller, const ::kfpanda::HttpRequest* request,
+              ::kfpanda::HttpResponse* response, ::google::protobuf::Closure* done) override;
+};
+
+inline void HttpKfPandaServiceImpl ::Echo(::google::protobuf::RpcController* controller,
+                                          const ::kfpanda::HttpRequest* request, ::kfpanda::HttpResponse* response,
+                                          ::google::protobuf::Closure* done) {
+  brpc::ClosureGuard dg(done);
+  brpc::Controller* cntl = static_cast<brpc::Controller*>(controller);
+  cntl->response_attachment().append(cntl->request_attachment());
+}
+inline void HttpKfPandaServiceImpl ::Replay(::google::protobuf::RpcController* controller,
+                                            const ::kfpanda::HttpRequest* request, ::kfpanda::HttpResponse* response,
+                                            ::google::protobuf::Closure* done) {
+  brpc::ClosureGuard dg(done);
+}
+}  // namespace kfpanda
