f7dfbbff4825ef1b438bac92bf4e2fff3d47b0c6
zhenkai.sun <zhenkai.sun@qq.com>
2025-05-10 21:11:15 +0800

    enhance api

---
+++ src/service/controllers/restful_cntl.h
@@ -0,0 +1,64 @@
+/**
+ * @file restful_cntl.h
+ * @brief
+ * @author zhenkai.sun
+ * @date 2025-05-10 11:34:33
+ */
+#pragma once
+
+#include <absl/status/status.h>
+#include <brpc/controller.h>
+
+#include <functional>
+#include <string>
+#include <unordered_map>
+
+#include "cppcommon/extends/rapidjson/builder.h"
+
+namespace kfpanda {
+class Response {
+ public:
+  Response() : status_(absl::OkStatus()) {}
+  explicit Response(const absl::Status &status) : status_(status) {}
+
+  template <typename K, typename V>
+  void Add(const K &k, const V &v);
+  template <typename K, typename V>
+  void AddJsonStr(const K &k, const V &v);
+  std::string ToJson();
+  std::string ToJson(const absl::Status &status);
+
+  static std::string From(const absl::Status &status);
+  template <typename V>
+  static std::string From(const absl::Status &status, const V &v);
+
+ private:
+  absl::Status status_;
+  cppcommon::JsonBuilder jb_;
+};
+
+template <typename K, typename V>
+void Response::Add(const K &k, const V &v) {
+  jb_.Add(k, v);
+}
+
+template <typename K, typename V>
+void Response::AddJsonStr(const K &k, const V &v) {
+  jb_.AddJsonStr(k, v);
+}
+
+template <typename V>
+std::string Response::From(const absl::Status &status, const V &v) {
+  Response res;
+  res.Add("data", v);
+  return res.ToJson(status);
+}
+
+using ViewFunc = std::function<absl::Status(brpc::Controller *cntl, Response *rsp)>;
+extern const std::unordered_map<std::string, ViewFunc> kRestfulApiViews;
+
+absl::Status api_echo(brpc::Controller *cntl, Response *rsp);
+absl::Status api_replay(brpc::Controller *cntl, Response *rsp);
+absl::Status api_debug_stat(brpc::Controller *cntl, Response *rsp);
+absl::Status api_debug_sample(brpc::Controller *cntl, Response *rsp);
+}  // namespace kfpanda
--- src/service/impl/http_impl.h
+++ src/service/impl/http_impl.h
@@ -9,14 +9,8 @@
 #include <string>
 
 #include "brpc/closure_guard.h"
-#include "common/flags.h"
-#include "cppcommon/extends/rapidjson/builder.h"
-#include "cppcommon/utils/to_str.h"
-#include "data/local/rocksdb_storage.h"
-#include "handler/record_handler.h"
-#include "handler/replay_handler.h"
 #include "protos/service/kfpanda/kfpanda.pb.h"
-#include "rapidjson/document.h"
+#include "service/controllers/restful_cntl.h"
 
 namespace kfpanda {
 class HttpKfPandaServiceImpl : public kfpanda::HttpKfPandaService {
@@ -33,19 +27,9 @@ inline void HttpKfPandaServiceImpl::Echo(::google::protobuf::RpcController* cont
                                          ::google::protobuf::Closure* done) {
   brpc::ClosureGuard dg(done);
   brpc::Controller* cntl = static_cast<brpc::Controller*>(controller);
-  cntl->response_attachment().append(cntl->request_attachment());
-  // spdlog::info("[{}] echo. [message={}]", __func__, cntl->request_attachment().to_string());
-
-  // record request
-  kfpanda::RecordRequest n_req;
-  kfpanda::RecordResponse n_resp;
-  n_req.set_service("KungFuPandaServer");
-  n_req.set_type(::kfpanda::RECORD_TYPE_HTTP);
-  n_req.mutable_uri()->set_path("/api/echo");
-  n_req.set_data(cntl->request_attachment().to_string());
-  auto status = RecordHandler::Handle(RecordContext{.cntl = controller, .request = &n_req, .response = &n_resp});
-  if (!status.ok()) {
-    RERROR("[{}] record faield", __func__);
+  auto s = api_echo(cntl, nullptr);
+  if (!s.ok()) {
+    RERROR("[{}] process faield. [message={}]", __func__, s.message());
   }
 }
 
@@ -54,29 +38,12 @@ inline void HttpKfPandaServiceImpl::Replay(::google::protobuf::RpcController* co
                                            ::google::protobuf::Closure* done) {
   brpc::ClosureGuard dg(done);
   brpc::Controller* cntl = static_cast<brpc::Controller*>(controller);
-  kfpanda::ReplayResponse rsp;
-  kfpanda::ReplayRequest req;
-
-  absl::Status status = absl::OkStatus();
-  if (cntl->request_attachment().empty()) {
-    req.set_service("KungFuPandaServer");
-    req.mutable_target()->set_host("127.0.0.1");
-    req.mutable_target()->set_port(FLAGS_port);
-    req.mutable_option()->set_count(1);
-  } else {
-    status = google::protobuf::util::JsonStringToMessage(cntl->request_attachment().to_string(), &req);
+  Response rsp;
+  auto s = api_replay(cntl, &rsp);
+  if (!s.ok()) {
+    RERROR("[{}] process faield. [message={}]", __func__, s.message());
   }
-  if (status.ok()) {
-    status = ReplayHandler::Handle(ReplayContext{.cntl = controller, .request = &req, .response = &rsp});
-  }
-  if (status.ok()) {
-    std::string js;
-    auto j = google::protobuf::util::MessageToJsonString(rsp, &js);
-    cntl->response_attachment().append(js);
-  } else {
-    auto r = fmt::format("code={}, message={}", static_cast<int>(status.code()), status.message());
-    cntl->response_attachment().append(r);
-  }
+  cntl->response_attachment().append(rsp.ToJson(s));
 }
 
 inline void HttpKfPandaServiceImpl::Api(::google::protobuf::RpcController* controller,
@@ -85,46 +52,15 @@ inline void HttpKfPandaServiceImpl::Api(::google::protobuf::RpcController* contr
   brpc::ClosureGuard dg(done);
   brpc::Controller* cntl = static_cast<brpc::Controller*>(controller);
   auto pt = cntl->http_request().uri().path();
-  if (pt == "/debug/stat") {
-    auto dbstat = RocksDbManager::GetDbState();
-    cntl->response_attachment().append(cppcommon::ToString(dbstat));
-  } else if (pt == "/debug/sample") {
-    auto r = cntl->request_attachment().to_string();
-    rapidjson::Document doc;
-    doc.Parse(r.c_str(), r.size());
-    if (doc.HasParseError()) {
-      cntl->response_attachment().append("parse request body failed (json)");
-    } else {
-      auto it_service = doc.FindMember("service");
-      auto it_count = doc.FindMember("count");
-      if (it_service == doc.MemberEnd()) {
-        cntl->response_attachment().append("service is not specified");
-      } else {
-        auto count = it_count == doc.MemberEnd() ? 1 : it_count->value.GetInt();
-        auto items = RocksDbManager::TryGetIterms(it_service->value.GetString(), count);
 
-        cppcommon::JsonBuilder jb;
-        for (auto& [k, v] : items) {
-          kfpanda::RecordRequest rr;
-          if (rr.ParseFromString(v)) {
-            if (rr.type() == kfpanda::RECORD_TYPE_HTTP) {
-              std::string njs;
-              auto s = google::protobuf::util::MessageToJsonString(rr, &njs);
-              if (s.ok()) {
-                jb.AddJsonStr(k, njs);
-              }
-            } else {
-              spdlog::info("[{}] unsupported protocol type", __func__);
-            }
-          } else {
-            spdlog::info("[{}] parse record request failed", __func__);
-          }
-        }
-        cntl->response_attachment().append(jb.Build());
-      }
-    }
+  auto it = kRestfulApiViews.find(pt);
+  auto status = absl::OkStatus();
+  if (it != kRestfulApiViews.end()) {
+    Response res;
+    auto status = it->second(cntl, &res);
+    cntl->response_attachment().append(res.ToJson(status));
   } else {
-    cntl->response_attachment().append("not impl");
+    cntl->response_attachment().append(Response::From(absl::ErrnoToStatus(410, "no such api")));
   }
 }
 }  // namespace kfpanda
