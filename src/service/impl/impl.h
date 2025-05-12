b50c5b6992a9763aea7cf85bbd21f55296359e14
Wii <zhenkai.sun@qq.com>
2025-05-12 15:59:03 +0800

    add sample api (#10)

--- src/service/impl/impl.h
+++ src/service/impl/impl.h
@@ -10,6 +10,8 @@ class KfPandaServiceImpl : public kfpanda::KfPandaService {
               ::kfpanda::RecordResponse* response, ::google::protobuf::Closure* done) override;
   void Replay(::google::protobuf::RpcController* controller, const ::kfpanda::ReplayRequest* request,
               ::kfpanda::ReplayResponse* response, ::google::protobuf::Closure* done) override;
+  void Sample(::google::protobuf::RpcController* controller, const ::kfpanda::SampleRequest* request,
+              ::kfpanda::SampleResponse* response, ::google::protobuf::Closure* done) override;
 };
 
 inline void KfPandaServiceImpl::Record(::google::protobuf::RpcController* controller,
@@ -28,5 +30,17 @@ inline void KfPandaServiceImpl::Replay(::google::protobuf::RpcController* contro
   auto status = ReplayHandler::Handle(ReplayContext{.cntl = controller, .request = request, .response = response});
   response->set_code(static_cast<int>(status.code()));
   response->set_message(status.message());
+}
+
+inline void KfPandaServiceImpl::Sample(::google::protobuf::RpcController* controller,
+                                       const ::kfpanda::SampleRequest* request, ::kfpanda::SampleResponse* response,
+                                       ::google::protobuf::Closure* done) {
+  brpc::ClosureGuard dg(done);
+  auto items = RocksDbManager::TryGetIterms(request->service(), request->count());
+  for (auto& item : items) {
+    response->add_data(item.second.data());
+  }
+  response->set_success(true);
+  response->set_code(0);
 }
 }  // namespace kfpanda
